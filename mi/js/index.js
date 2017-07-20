da4d83d887a315998f0769c10415af04d2f09a7f
亚敏 <lym0819a@163.com>
2017-07-21 07:55:10 +0800

    add

---
+++ mi/js/index.js
@@ -0,0 +1,12 @@
+;(function(){
+	var swiper=new Swiper(".swiper-container",{
+		autoplay:1000,
+		loop:true,
+		autoplayDisableOnInteraction:false,
+        pagination: '.swiper-pagination',
+        paginationClickable: true	
+	})
+
+	
+	
+})(jQuery);
